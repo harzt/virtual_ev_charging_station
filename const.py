3677fc3c7aba403f8fdd5cebc5d272724c7b3b77
harzt <48691508+harzt@users.noreply.github.com>
2026-06-06 11:17:35 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,11 @@
+DOMAIN = "virtual_ev_charging_station"
+
+CONF_ENCHUFE = "enchufe_switch"
+CONF_ENERGIA = "enchufe_energia"
+CONF_POTENCIA = "enchufe_potencia"
+CONF_SOLAR = "sensor_solar"
+CONF_CAPACIDAD = "capacidad_bateria"
+CONF_POTENCIA_CARGA = "potencia_carga"
+CONF_UMBRAL_SOLAR = "umbral_solar"
+
+PLATFORMS = ["number", "switch", "sensor"]
